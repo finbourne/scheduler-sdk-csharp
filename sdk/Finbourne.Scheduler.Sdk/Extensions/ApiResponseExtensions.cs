f28141bf81db852a4b51d679d3cf1b7b1ce7ddce
Concourse <concourse@finbourne.com>
2023-11-03 11:44:17 +0000

    Updates for v2.0.1

--- sdk/Finbourne.Scheduler.Sdk/Extensions/ApiResponseExtensions.cs
+++ sdk/Finbourne.Scheduler.Sdk/Extensions/ApiResponseExtensions.cs
@@ -18,7 +18,7 @@ namespace Finbourne.Scheduler.Sdk.Extensions
         /// <summary>
         /// The header that the request id is contained within.
         /// </summary>
-        public const string RequestIdHeader = "lusid-meta-requestid";
+        public const string RequestIdHeader = "scheduler-meta-requestid";
         
         /// <summary>
         /// The header that the Date Time Offset is contained within.
