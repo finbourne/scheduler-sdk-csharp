6b9818ecd4f8a5a72ac865ada1c7cd3560e5bab4
Concourse <concourse@finbourne.com>
2024-03-12 19:51:36 +0000

    Updates for v2.0.18

--- sdk/Finbourne.Scheduler.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Scheduler.Sdk/Client/Configuration.cs
@@ -543,7 +543,7 @@ namespace Finbourne.Scheduler.Sdk.Client
             string report = "C# SDK (Finbourne.Scheduler.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.0.866\n";
+            report += "    Version of the API: 0.0.867\n";
             report += "    SDK Package Version: 2.0.0\n";
 
             return report;
