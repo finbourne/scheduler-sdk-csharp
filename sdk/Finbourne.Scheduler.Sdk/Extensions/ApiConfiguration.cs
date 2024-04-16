f48ebc69fef1c8303a0cb4f2a9dca49aa548360b
Concourse <concourse@finbourne.com>
2024-04-16 14:36:11 +0000

    Updates for v2.0.31

--- sdk/Finbourne.Scheduler.Sdk/Extensions/ApiConfiguration.cs
+++ sdk/Finbourne.Scheduler.Sdk/Extensions/ApiConfiguration.cs
@@ -45,8 +45,15 @@ namespace Finbourne.Scheduler.Sdk.Extensions
         /// <summary>
         /// Finbourne-scheduler Api Url
         /// </summary>
-        [ConfigurationKeyName("finbourne-schedulerUrl")]
+        [ConfigurationKeyName("finbourne_schedulerUrl")]
         public string BaseUrl { get; set; }
+
+
+        /// <summary>
+        /// finbourne_scheduler Api Url
+        /// </summary>
+        [ConfigurationKeyName("finbourne-schedulerUrl")]
+        public string BackUpBaseUrl { get; set; }
 
         /// <summary>
         /// Client Application name
@@ -61,7 +68,7 @@ namespace Finbourne.Scheduler.Sdk.Extensions
 
         internal bool MissingPersonalAccessTokenVariables =>
             string.IsNullOrWhiteSpace(PersonalAccessToken) ||
-            string.IsNullOrWhiteSpace(BaseUrl);
+            (string.IsNullOrWhiteSpace(BaseUrl) && string.IsNullOrWhiteSpace(BackUpBaseUrl));
 
         internal bool MissingSecretVariables =>
             string.IsNullOrWhiteSpace(TokenUrl) ||
@@ -69,7 +76,7 @@ namespace Finbourne.Scheduler.Sdk.Extensions
             string.IsNullOrWhiteSpace(Password) ||
             string.IsNullOrWhiteSpace(ClientId) ||
             string.IsNullOrWhiteSpace(ClientSecret) ||
-            string.IsNullOrWhiteSpace(BaseUrl);
+            (string.IsNullOrWhiteSpace(BaseUrl) && string.IsNullOrWhiteSpace(BackUpBaseUrl)) ;
 
         /// <summary>
         /// Checks if any of the required configuration values are missing
@@ -122,14 +129,14 @@ namespace Finbourne.Scheduler.Sdk.Extensions
                 missingConfig.Add(nameof(ClientSecret));
             }
 
-            if (string.IsNullOrWhiteSpace(BaseUrl))
+            if(string.IsNullOrWhiteSpace(BackUpBaseUrl) && string.IsNullOrWhiteSpace(BaseUrl))
             {
-                PropertyInfo propertyInfo = typeof(ApiConfiguration).GetProperty(nameof(BaseUrl));
+                PropertyInfo propertyInfo = typeof(ApiConfiguration).GetProperty(nameof(BackUpBaseUrl));
                 var attribute = (ConfigurationKeyNameAttribute)Attribute.GetCustomAttribute(propertyInfo!, typeof(ConfigurationKeyNameAttribute));
 
                 missingConfig.Add(attribute!.Name);
             }
-
+            
             return missingConfig;
         }  
     }
--- sdk/Finbourne.Scheduler.Sdk/Extensions/ApiConfigurationBuilder.cs
+++ sdk/Finbourne.Scheduler.Sdk/Extensions/ApiConfigurationBuilder.cs
@@ -32,7 +32,7 @@ namespace Finbourne.Scheduler.Sdk.Extensions
         private static readonly Dictionary<string, string> ConfigNamesToSecrets = new Dictionary<string, string>()
         {
             { "TokenUrl", "tokenUrl" },
-            { "BaseUrl", "finbourne-schedulerUrl" },
+            { "BaseUrl", "finbourne_schedulerUrl" },
             { "ClientId", "clientId" },
             { "ClientSecret", "clientSecret" },
             { "Username", "username" },
@@ -68,6 +68,8 @@ namespace Finbourne.Scheduler.Sdk.Extensions
             var apiConfig = new ApiConfiguration();
             config.Bind(apiConfig);
 
+            apiConfig = CheckBaseUrl(apiConfig);
+
             if (apiConfig.HasMissingConfig())
             {
                 var missingValues = apiConfig.GetMissingConfig()
@@ -85,7 +87,10 @@ namespace Finbourne.Scheduler.Sdk.Extensions
             var apiConfig = new ApiConfiguration
             {
                 TokenUrl = Environment.GetEnvironmentVariable("FBN_TOKEN_URL") ?? Environment.GetEnvironmentVariable("fbn_token_url"),
-                BaseUrl = Environment.GetEnvironmentVariable("FBN_FINBOURNE-SCHEDULER_API_URL") ?? Environment.GetEnvironmentVariable("fbn_finbourne-scheduler_api_url"),
+                BaseUrl = (Environment.GetEnvironmentVariable("FBN_FINBOURNE-SCHEDULER_API_URL") ?? 
+                           Environment.GetEnvironmentVariable("fbn_finbourne-scheduler_api_url")) ?? 
+                          (Environment.GetEnvironmentVariable("FBN_FINBOURNE_SCHEDULER_API_URL") ?? 
+                           Environment.GetEnvironmentVariable("fbn_finbourne_scheduler_api_url")),
                 ClientId = Environment.GetEnvironmentVariable("FBN_CLIENT_ID") ?? Environment.GetEnvironmentVariable("fbn_client_id"),
                 ClientSecret = Environment.GetEnvironmentVariable("FBN_CLIENT_SECRET") ?? Environment.GetEnvironmentVariable("fbn_client_secret"),
                 Username = Environment.GetEnvironmentVariable("FBN_USERNAME") ?? Environment.GetEnvironmentVariable("fbn_username"),
@@ -119,6 +124,8 @@ namespace Finbourne.Scheduler.Sdk.Extensions
                             .Build();
             config.GetSection("api").Bind(apiConfig);
 
+            apiConfig = CheckBaseUrl(apiConfig);
+
             if (apiConfig.HasMissingConfig())
             {
                 var missingValues = apiConfig.GetMissingConfig()
@@ -129,6 +136,16 @@ namespace Finbourne.Scheduler.Sdk.Extensions
             }
 
             return apiConfig;
+        }
+
+        private static ApiConfiguration CheckBaseUrl(ApiConfiguration parsedConfig) 
+        {
+            if(string.IsNullOrWhiteSpace(parsedConfig.BaseUrl))
+            {
+                parsedConfig.BaseUrl = parsedConfig.BackUpBaseUrl;
+            }
+
+            return parsedConfig;
         }
     }
 }
